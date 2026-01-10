a1ddb98da94ba811c54b87c6d80ca4b006f52997
Nero Bratti <bratti.nero@gmail.com>
2026-01-10 20:06:14 -0300

    Upload files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new Frame();
+
+        //String addressesPath = "addresses/virtual4bits.txt";
+        //Simulator simulator = new Simulator();
+        //simulator.run(addressesPath);
+    }
+}
