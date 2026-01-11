9ef239d53adb59e0d237bfa1e1d389ee9539c202
Maria Eduarda Weiland Machado Bratti <bratti.nero@gmail.com>
2026-01-10 22:29:03 -0300

    Testing git

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
+import ui.Frame;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         JFrame frame = new Frame();
-
-        //String addressesPath = "addresses/virtual4bits.txt";
-        //Simulator simulator = new Simulator();
-        //simulator.run(addressesPath);
     }
 }
---
+++ src/ui/Frame.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.*;
+
+public class Frame extends JFrame {
+    public Frame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(800, 600);
+        this.setTitle("Simulação do escalonamento de processos");
+        this.setVisible(true);
+    }
+}
